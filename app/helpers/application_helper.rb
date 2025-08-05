c7c5c8a403e9cc89c05eca17a9cbf331c37527a7
Andres Muñoz <andrefe91@gmail.com>
2025-08-05 15:48:08 +0800

    Project page information and some adjustments to titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def projects_hash
+    # The memoization helps to cache the information
+    @projects_hash ||= YAML.safe_load(File.read(Rails.root.join('config/data', 'projects.yaml')), aliases: true)
+  end
 end
